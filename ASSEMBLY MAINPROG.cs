d7caf20b851094477ec8d051497938eb69ce0ea3
h587736 <54101886+h587736@users.noreply.github.com>
2023-03-09 16:13:59 +0100

    Additional field
    Added addition fields with dummy values

--- ASSEMBLY MAINPROG.cs
+++ ASSEMBLY MAINPROG.cs
@@ -7,16 +7,16 @@ class Astronomy {
 	{
 		List<SpaceObject> solarSystem = new List<SpaceObject>
 		{
-			new Star("Sun", 0.0046491),
-			new Planet("Mercury", 0.3870989),
-			new Planet("Venus", 0.723331),
-			new Planet("Terra", 1),
-			new Moon("The moon", 0.00257),
+			new Star("Sun", 110852,0,0,0,"White"),
+			new Planet("Mercury", 9212,0,0,0,"Slate gray"),
+			new Planet("Venus", 17238,0,0,0,"Yellow-white"),
+			new Planet("Terra", 23737,0,0,0,"Blue-green"),
+			new Moon("The moon", 61,0,0,0,"off-white brown-gray"),
 			//Task 2
-            new DwarfPlanet("Pluto", 0.039482),
-            new Comet("Anita cox's Comet", 6.2),
-            new Asteroid("Ann al", 2.5),
-            new AsteroidBelt("Main Belt", 2.7)
+            new DwarfPlanet("Pluto", 940,0,0,0,"white, tan and brownish-red"),
+            new Comet("Anita cox's Comet", 500000,0,0,0,"Green"),
+            new Asteroid("Ann al", 200000,0,0,0,"White"),
+            new AsteroidBelt("Main Belt", 419354175,0,0,0,"Gray")
         };
 		foreach (SpaceObject @obj in solarSystem)
 		{
--- ASSEMBLY SPACEOBJECT.cs
+++ ASSEMBLY SPACEOBJECT.cs
@@ -7,90 +7,110 @@ namespace SpaceSim
 
         protected String name;
         protected double OrbRadius;
+        protected double OrbPeriod;
+        protected double ObjRadius;
+        protected double RotPeriod;
+        protected String ObjColor;
 
 
-    public SpaceObject(string name, double OrbRadius)
+    public SpaceObject(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor)
+        {
+            this.name = name;
+            this.OrbRadius = OrbRadius;
+            this.OrbPeriod = OrbPeriod;
+            this.ObjRadius = ObjRadius;
+            this.RotPeriod = RotPeriod;
+            this.ObjColor = ObjColor;
+        }
+        public virtual void Draw()
     {
-        this.name = name;
-        this.OrbRadius = OrbRadius; 
-    }
-    public virtual void Draw()
-    {
-        Console.WriteLine(name);
-    }
+        Console.WriteLine(name 
+            + "\n Orbital Radius in KM: " + OrbRadius 
+            + "\n Orbital Period: " + OrbPeriod
+            + "\n Object Radius: " + ObjRadius
+            + "\n Rotational Period: " + RotPeriod
+            + "\n Object Color: " + ObjColor);
+        }
 }
 
 public class Star : SpaceObject
 {
-    public Star(string name, double OrbRadius) : base(name, OrbRadius) { }
+    public Star(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor) : base(name, OrbRadius, OrbPeriod, ObjRadius, RotPeriod, ObjColor) { }
     public override void Draw()
     {
         Console.Write("Star: ");
-        base.Draw();
+             base.Draw();
+           
     }
 }
 
 public class Planet : SpaceObject
 {
-    public Planet(string name, double OrbRadius) : base(name, OrbRadius) { }
-    public override void Draw()
+    public Planet(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor) : base(name, OrbRadius, OrbPeriod, ObjRadius, RotPeriod, ObjColor) { }
+        public override void Draw()
     {
         Console.Write("Planet: ");
-        base.Draw();
-    }
+             base.Draw();
+            
+        }
 }
 
 public class Moon : Planet
 {
-    public Moon(string name, double OrbRadius) : base(name, OrbRadius) { }
+    public Moon(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor) : base(name, OrbRadius, OrbPeriod, ObjRadius, RotPeriod, ObjColor) { }
 
     public override void Draw()
     {
         Console.Write("Moon: ");
-        base.Draw();
-    }
+            // base.Draw();
+            base.Draw();
+        }
 }
     //Task 2
     public class Comet : SpaceObject
     {
-        public Comet(string name, double OrbRadius) : base(name, OrbRadius) { }
+        public Comet(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor) : base(name, OrbRadius, OrbPeriod, ObjRadius, RotPeriod, ObjColor) { }
 
         public override void Draw()
         {
             Console.Write("Comet: ");
+            // base.Draw();
             base.Draw();
         }
     }
 
     public class Asteroid : SpaceObject
     {
-        public Asteroid(string name, double OrbRadius) : base(name, OrbRadius) { }
+        public Asteroid(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor) : base(name, OrbRadius, OrbPeriod, ObjRadius, RotPeriod, ObjColor) { }
 
         public override void Draw()
         {
             Console.Write("Asteroid: ");
+            // base.Draw();
             base.Draw();
         }
     }
 
     public class AsteroidBelt : SpaceObject
     {
-        public AsteroidBelt(string name, double OrbRadius) : base(name, OrbRadius) { }
+        public AsteroidBelt(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor) : base(name, OrbRadius, OrbPeriod, ObjRadius, RotPeriod, ObjColor) { }
 
         public override void Draw()
         {
             Console.Write("Asteroid Belt: ");
+            // base.Draw();
             base.Draw();
         }
     }
 
     public class DwarfPlanet : SpaceObject
     {
-        public DwarfPlanet(string name, double OrbRadius) : base(name, OrbRadius) { }
+        public DwarfPlanet(string name, double OrbRadius, double OrbPeriod, double ObjRadius, double RotPeriod, String ObjColor) : base(name, OrbRadius, OrbPeriod, ObjRadius, RotPeriod, ObjColor) { }
 
         public override void Draw()
         {
             Console.Write("Dwarf Planet: ");
+           // base.Draw();
             base.Draw();
         }
     }
