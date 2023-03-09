b69773a9aba36593e8a836a47f02fbafbc6ded21
h587736 <54101886+h587736@users.noreply.github.com>
2023-03-09 14:55:48 +0100

    Initial
    Initial commit (Task 1 & 2)

---
+++ ASSEMBLY MAINPROG.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using SpaceSim;
+
+class Astronomy {
+	public static void Main()
+	{
+		List<SpaceObject> solarSystem = new List<SpaceObject>
+		{
+			new Star("Sun"),
+			new Planet("Mercury"),
+			new Planet("Venus"),
+			new Planet("Terra"),
+			new Moon("The moon"),
+			//Task 2
+            new DwarfPlanet("Pluto"),
+            new Comet("Anita cox's Comet"),
+            new Asteroid("Ann al"),
+            new AsteroidBelt("The asteroid belt")
+        };
+		foreach (SpaceObject @obj in solarSystem)
+		{
+			obj.Draw();
+		}
+		Console.ReadLine();
+	}
+}
---
+++ ASSEMBLY SPACEOBJECT.cs
@@ -0,0 +1,97 @@
+﻿using SpaceSim;
+using System;
+
+namespace SpaceSim
+{
+    public class SpaceObject {
+
+        protected String name;
+    
+
+
+    public SpaceObject(string name)
+    {
+        this.name = name;
+    }
+    public virtual void Draw()
+    {
+        Console.WriteLine(name);
+    }
+}
+
+public class Star : SpaceObject
+{
+    public Star(string name) : base(name) { }
+    public override void Draw()
+    {
+        Console.Write("Star: ");
+        base.Draw();
+    }
+}
+
+public class Planet : SpaceObject
+{
+    public Planet(string name) : base(name) { }
+    public override void Draw()
+    {
+        Console.Write("Planet: ");
+        base.Draw();
+    }
+}
+
+public class Moon : Planet
+{
+    public Moon(string name) : base(name) { }
+
+    public override void Draw()
+    {
+        Console.Write("Moon: ");
+        base.Draw();
+    }
+}
+    //Task 2
+    public class Comet : SpaceObject
+    {
+        public Comet(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.Write("Comet: ");
+            base.Draw();
+        }
+    }
+
+    public class Asteroid : SpaceObject
+    {
+        public Asteroid(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.Write("Asteroid: ");
+            base.Draw();
+        }
+    }
+
+    public class AsteroidBelt : SpaceObject
+    {
+        public AsteroidBelt(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.Write("Asteroid Belt: ");
+            base.Draw();
+        }
+    }
+
+    public class DwarfPlanet : SpaceObject
+    {
+        public DwarfPlanet(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.Write("Dwarf Planet: ");
+            base.Draw();
+        }
+    }
+
+}
