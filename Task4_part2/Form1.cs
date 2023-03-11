0119cb04401c6af6b7d84e1f736d2b84b5a04858
h587736 <54101886+h587736@users.noreply.github.com>
2023-03-11 17:09:32 +0100

    Task 4 Part 2

---
+++ Task4_part2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using SpaceSim;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task4_part2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            Brush b = new SolidBrush(Color.Yellow);
+
+            g.FillEllipse(b, 100, 100, 100, 100);
+        }
+    }
+}
+    
+
